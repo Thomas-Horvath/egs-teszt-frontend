79ac306f6aead28e74fae71783f1e6a9b356358f
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-07-29 12:38:00 +0200

    restructuring project

--- scripts/updateproduct.js
+++ scripts/updateproduct.js
@@ -1,6 +1,6 @@
 // const url = "https://thomasapi.eu"
 // const url = "http://127.0.0.1:9000"
-const url = "https://egs-backend.onrender.com/"
+const url = "https://egs-backend.onrender.com"
 const params = new URLSearchParams(window.location.search);
 const productId = params.get('id');
 
